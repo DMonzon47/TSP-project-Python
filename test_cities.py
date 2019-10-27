b811f4b29b8bd7f9c728f756bc8b77d0f8698228
Emma Paris <dmonzo01@mail.bbk.ac.uk>
2019-10-27 19:14:34 +0000

    function tests and stubs

--- test_cities.py
+++ test_cities.py
@@ -10,53 +10,22 @@ def test_compute_total_distance():
     assert compute_total_distance(road_map1)==\
            pytest.approx(9.386+18.496+10.646, 0.01)
 
-    '''add your further tests'''
+    assert (compute_total_distance(2) == 4.1)
+    #assert.raises(TypeError, compute_total_distance, "4.1")
 
 def test_swap_cities():
     '''add your tests'''
+    assert (swap_cities(1,4) == ("test1", "new_total_distance"))
+    #assert.raise(TypeError, swap_cities, True)
 
 def test_shift_cities():
     '''add your tests'''
+    assert shift_cities("road_map") == ("road_map")
+    #assert.raise(TypeError, shift_cities, 5)
 
-
-
-"""""
-def test_compute_total_distance(road_map):
-    """
-    Returns, as a floating point number, the sum of the distances of all
-    the connections in the `road_map`. Remember that it's a cycle, so that
-    (for example) in the initial `road_map`, Wyoming connects to Alabama...
-    """
-    pass
-"""""
-
-
-"""""
-def test_swap_cities(road_map, index1, index2):
-    """
-    Take the city at location `index` in the `road_map`, and the
-    city at location `index2`, swap their positions in the `road_map`,
-    compute the new total distance, and return the tuple
-
-        (new_road_map, new_total_distance)
-
-    Allow for the possibility that `index1=index2`,
-    and handle this case correctly.
-    """
-    pass
-"""""
-
-"""""
-def test_shift_cities(road_map):
-    """
-    For every index i in the `road_map`, the city at the position i moves
-    to the position i+1. The city at the last position moves to the position
-    0. Return the new road map.
-    """
-    pass
-"""""
-
-
+"""
 if test___name__ == "__main__": #keep this in
     main()
-
+    assert __name__("__main__") == "__main__"
+    #assert.raise(TypeError, __name__, 5)
+"""
