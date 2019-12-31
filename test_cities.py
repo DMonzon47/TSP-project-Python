ebbc7a9bd0135a0d75c81194ebafff6e2fdeeedf
Delvin Monzon <dmonzo01@mail.bbk.ac.uk>
2019-12-31 15:51:28 +0000

    Updated test_cities.py and function tests. Need to fix implementation in 3 functions found in test_cities.py and cities2.ipynb

--- test_cities.py
+++ test_cities.py
@@ -6,10 +6,25 @@ def test_compute_total_distance():
                 ("Delaware", "Dover", 39.161921, -75.526755),\
                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
     
-    assert compute_total_distance(road_map1)==\
-           pytest.approx(9.386+18.496+10.646, 0.01)
+    road_map2 = [("Kentucky", "Frankfort", '38.197274', '-84.86311'),\
+                ("Delaware", "Dover", '39.161921', '-75.526755'),\
+                ("Minnesota", "Saint Paul", '44.95', '-93.094')]
+        
+    road_map3 = []
+    
+    road_map4 = [("Kentucky", "Frankfort", int(38.197274), int(-84.86311)),\
+                ("Delaware", "Dover", 39.161921, -75.526755),\
+                ("Minnesota", "Saint Paul", 44.95, -93.094)]
 
-    assert compute_total_distance(road_map1) == pytest.approx(4.1)
+    assert cities.compute_total_distance(road_map1) == pytest.approx(38.53)
+    
+    assert cities.compute_total_distance(road_map2) == "Incorrect type. 'road_map' index[2] and [3] must be of float() types."
+    
+    assert cities.compute_total_distance(road_map3)== "Empty 'road_map', please input correct format: [('state','city','latitude','longitude')]."
+    assert cities.compute_total_distance(road_map4) == "Incorrect type. 'road_map' index[2] and [3] must be of float() types."
+    
+    
+    
     
 def test_swap_cities():
 
@@ -20,12 +35,31 @@ def test_swap_cities():
     new_road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
                 ("Delaware", "Dover", 39.161921, -75.526755),\
                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
+    
+    
+    road_map2 = [("Kentucky", "Frankfort", '38.197274', '-84.86311'),\
+                ("Delaware", "Dover", '39.161921', '-75.526755'),\
+                ("Minnesota", "Saint Paul", '44.95', '-93.094')]
+        
+    road_map3 = []
+    
+    road_map4 = [("Kentucky", "Frankfort", int(38.197274), int(-84.86311)),\
+                ("Delaware", "Dover", 39.161921, -75.526755),\
+                ("Minnesota", "Saint Paul", 44.95, -93.094)]
 
     new_total_distance = pytest.approx(4.4)
 
-    assert swap_cities(road_map1,[0],[1]) == (new_road_map1, new_total_distance)
+    assert cities.swap_cities(road_map1,0,1) == (new_road_map1, new_total_distance)
                         #assert.raise(TypeError, swap_cities, True)
-
+    assert cities.swap_cities(road_map1,1.4,2.8) == "Input: index1 or index2 is of wrong type. Input must be an int."
+    
+    assert cities.swap_cities(road_map2,1,2) == "Incorrect type. 'road_map' index[2] and [3] must be of float() types."
+    
+    assert cities.swap_cities(road_map3,1,2)== "Empty 'road_map', please input correct format: [('state','city','latitude','longitude')]."
+    assertcities.swap_cities(road_map4,1,2) == "Incorrect type. 'road_map' index[2] and [3] must be of float() types."
+    
+    
+    
 def test_shift_cities():
     road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
                 ("Delaware", "Dover", 39.161921, -75.526755),\
@@ -34,5 +68,28 @@ def test_shift_cities():
     new_road_map2 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
                 ("Delaware", "Dover", 39.161921, -75.526755),\
                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
-    assert shift_cities(road_map1) == new_road_map2
+        road_map2 = [("Kentucky", "Frankfort", '38.197274', '-84.86311'),\
+                ("Delaware", "Dover", '39.161921', '-75.526755'),\
+                ("Minnesota", "Saint Paul", '44.95', '-93.094')]
+        
+    road_map3 = []
+    
+    road_map4 = [("Kentucky", "Frankfort", int(38.197274), int(-84.86311)),\
+                ("Delaware", "Dover", 39.161921, -75.526755),\
+                ("Minnesota", "Saint Paul", 44.95, -93.094)]
+
+    new_total_distance = pytest.approx(4.4)
+
+    assert cities.shift_cities(road_map1) == new_road_map2
                 #assert.raise(TypeError, shift_cities, 5)
+        
+    assert cities.shift_cities(road_map1) == (new_road_map1, new_total_distance)
+                        #assert.raise(TypeError, swap_cities, True)
+    
+    assert cities.shift_cities(road_map2) == "Incorrect type. 'road_map' index[2] and [3] must be of float() types."
+    
+    assert cities.shift_cities(road_map3)== "Empty 'road_map', please input correct format: [('state','city','latitude','longitude')]."
+    assert cities.shift_cities(road_map4) == "Incorrect type. 'road_map' index[2] and [3] must be of float() types."
+
+        
+    
